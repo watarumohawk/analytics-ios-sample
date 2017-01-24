6ed96ff4a93b03b3e07c05f1468d2c6f5c7f5d66
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-01-24 14:36:49 +0900

    added GoogleTagManager

--- BridgingHeader.h
+++ BridgingHeader.h
@@ -6,4 +6,15 @@
 //  Copyright © 2016年 Wataru Sekiguchi. All rights reserved.
 //
 
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
 #import <Google/Analytics.h>
+#import <GoogleTagManager/TAGManager.h>
+#import <GoogleTagManager/TAGContainer.h>
+#import <GoogleTagManager/TAGContainerOpener.h>
+#import <GoogleTagManager/TAGDataLayer.h>
+#import <GoogleTagManager/TAGLogger.h>
+
+#endif /* BridgingHeader_h */
